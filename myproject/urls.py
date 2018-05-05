201e13ef730f642bc3172afb73b8e7614213c5aa
girvar <girvarsingh2000@gmail.com>
2018-05-05 17:18:05 +0530

    template view details

--- myproject/urls.py
+++ myproject/urls.py
@@ -17,8 +17,8 @@ from django.conf.urls import include
 from django.conf.urls import url
 from django.contrib import admin
 
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('blog.urls')),
+    
 ]
